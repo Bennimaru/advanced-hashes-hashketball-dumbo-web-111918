25ad7db4a56b83b11a521b92841a9911989b1c18
Benson Yu <bennywhyu@gmail.com>
2018-11-14 20:13:00 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,8 @@ def num_points_scored(players_name)
         stats.collect{|type,value|
           if type == :points
             points << value
-          end 
+          end
+          binding.pry
         }
       end  
     }
